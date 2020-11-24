37c8314ad74f85b5d2a81a124ef0c6bc28a333a5
袁帅 <yuanshuai@cmict.chinamobile.com>
2020-11-24 11:32:41 +0800

    make pic controllable by wasd

--- core/src/com/badlogic/drop/Drop.java
+++ core/src/com/badlogic/drop/Drop.java
@@ -2,27 +2,60 @@ package com.badlogic.drop;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+
 
 public class Drop extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	private OrthographicCamera camera;
+	private Rectangle bucket;
+
+	private float speed = 200L;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+
+		bucket = new Rectangle();
+		bucket.x = 800 / 2 - 64 / 2; // center the bucket horizontally
+		bucket.y = 20; // bottom left corner of the bucket is 20 pixels above the bottom screen edge
+		bucket.width = 64;
+		bucket.height = 64;
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		camera.update();
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, bucket.x, bucket.y);
 		batch.end();
+
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			bucket.x -= speed * Gdx.graphics.getDeltaTime();
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			bucket.x += speed * Gdx.graphics.getDeltaTime();
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.UP)) {
+			bucket.y += speed * Gdx.graphics.getDeltaTime();
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+			bucket.y -= speed * Gdx.graphics.getDeltaTime();
+		}
+
 	}
 	
 	@Override
